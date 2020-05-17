1e8ad36b6927f2194cb3a7fdcf22eb3c93662d78
drspllinter <63166626+drspllinter@users.noreply.github.com>
2020-05-17 21:55:18 +0200

    Update cholesky4x4.c

--- cholesky4x4.c
+++ cholesky4x4.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-using namespace std;
+#inlude <iostream>
 int main(int argc, char** argv) {
   // Initialize the MPI environment
   MPI_Init(NULL, NULL);
@@ -28,7 +28,7 @@ int main(int argc, char** argv) {
 		}
 		printf("\n");
 		a=sqrt(M[0][1]);
-		cout<<a<<endl;
+		std::cout<<a;
 		MPI_Send(&a, 1, MPI_FLOAT, 1, 0, MPI_COMM_WORLD);     
 	}
 	else if (world_rank==1){
