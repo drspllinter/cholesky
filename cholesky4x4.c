c2c48c67f73bcda5ad9d61f1e002de4fa498a66e
drspllinter <63166626+drspllinter@users.noreply.github.com>
2020-05-17 23:56:18 +0200

    Update cholesky4x4.c

--- cholesky4x4.c
+++ cholesky4x4.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-#inlude <iostream>
+#include <iostream>
 int main(int argc, char** argv) {
   // Initialize the MPI environment
   MPI_Init(NULL, NULL);
