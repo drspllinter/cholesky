464067ef3f8e566fc2a208a3e22e7702494dfde2
drspllinter <63166626+drspllinter@users.noreply.github.com>
2020-05-18 18:00:04 +0200

    Update cholesky4x4.c

--- cholesky4x4.c
+++ cholesky4x4.c
@@ -15,7 +15,7 @@ int main(int argc, char** argv) {
   double a;
   double b;
   double M[3][3] = {{4, 12, -16}, {12, 37, -43}, {-16, -43, 98}};
-  for (int k=0; k<1; k++)//k indeks kolumny (algorytm idzie po dolnej diagonali od L do P)
+  for (int k=0; k<3; k++)//k indeks kolumny (algorytm idzie po dolnej diagonali od L do P)
   {		  
 	if(world_rank == 0){
 		
@@ -35,33 +35,42 @@ int main(int argc, char** argv) {
 		M[k][k]=sqrt(M[k][k]);
 		for (int p=1; p<n-k; p++) //p indeks procesora do ktorego wysylamy 
 		{	
-			for (int c=0; c<=k; c++)//c indeks columny elementow do wysylki 
+			for (int c=0; c<=k; c++)//petla wysylajace potrzebne do obliczen wielkosci, c indeks columny elementow do wysylki 
 			{
 				for (int r=c; r<n; r++)//r indeks wiersza elementu do wysylki
 				{	
 					MPI_Send(&M[r][c], 1, MPI_DOUBLE, p, 0, MPI_COMM_WORLD);
 				}
 			}
+			MPI_Recv(&M[p+k][k], 1, MPI_DOUBLE, p, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
 		}	
 			
-		/*printf("Po dzialaniach: \n");
+		printf("Po dzialaniach: \n");
 		for (int i = 0; i <n; i++){
 			for (int j = 0; j <n; j++){
 				printf("%f ,", M[i][j]);
 			}
 			printf("\n");
-		}*/    
+		}    
 	}
 	else if (world_rank<n){
 		//printf("Jestem procesorem: %d\n", world_rank);
-		for (int c=0; c<=k; c++)
+		for (int c=0; c<=k; c++)//petla przyjmujaca potrzebne do obliczen wartosci
 		{
 			for (int r=c; r<n; r++)
 			{
 				MPI_Recv(&M[r][c], 1, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-				printf("Jestem procesorem %d i otrzymalem element M[%d][%d]=%f", world_rank, r, c, M[r][c]);
+				//printf("Jestem procesorem %d z kolumny %d i otrzymalem element M[%d][%d]=%f\n", world_rank, k, r, c, M[r][c]);
 			}
 		}
+		
+		for (int g=0; g<k; g++)
+		{
+			M[k+world_rank][k]-=M[k][g]*M[k+world_rank][g];		
+		}	
+		M[k+world_rank][k]/=M[k][k];
+		MPI_Send(&M[k+world_rank][k], 1, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD);
+		
 		/*printf("Jestem procesorem %d i moja macierz to:\n", world_rank);
 			for (int i = 0; i <n; i++)
 			{
